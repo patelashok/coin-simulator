63ad39ad62328b63e645aa52613f8527304d706e
patelashok <ashok.patel2917@gmail.com>
2021-05-03 16:34:10 +0530

    Multiple times filpping coin show winner

--- flipcoinSimulator.sh
+++ flipcoinSimulator.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
-echo "Welcome to coin simulator
+Tails=0
+Heads=0
+echo "Welcome to coin simulator"
+for (( i=1;i<10;i++))
+do
+	R=$((RANDOM%2))
+	if [ $R -eq 0 ]
+	then
+	((Tails++))
+	else
+	((Heads++))
+	fi
+done
+echo "Talis and Heads value: $Tails ,$Heads"
+if [ $Heads -gt $Tails ]
+then
+	echo "Winner is Heads"
+else
+	echo "Winner is Tails"
+fi
